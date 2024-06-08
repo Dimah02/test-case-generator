5b006c1dee101e467726bafae257a2bf772fe57b
dimah02 <dimahmehdawi@gmail.com>
2024-06-08 17:45:07 +0300

    Finish UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,88 @@
 <script>
-    import image from '$lib/images/mechanic.png';
+    import { writableArray } from "$lib/store/list-store.js";
+    import { IntegerTest } from "$lib/models/IntegerTest.js";
+
+    const addInteger = () => {
+        $writableArray = [
+            ...$writableArray,
+            {
+                type: "Integer",
+                value: new IntegerTest(1, 10),
+            },
+        ];
+    };
 </script>
 
-<div>
-    <img alt="Working now" src={image} width="400" />
-    <h1 class="text-5xl text-white">Test Cases Generator</h1>
-    <h1 class="text-3xl text-white">I am working on it</h1>
+<div id="container">
+    <h1
+        class="mt-16 mb-8 text-3xl font-bold md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-400 from-sky-400"
+    >
+        Test Case Generator
+    </h1>
+
+    <ul class="menu menu-horizontal bg-base-200 rounded-box w-3/6 p-2 mb-6">
+        <li><button class="text-lg" on:click={addInteger}>Integer</button></li>
+    </ul>
+
+    <div id="container2" class="bg-base-200 rounded-box mb-6">
+        <div id="first" class="w-3/6">
+            {#each $writableArray as item}
+                <div id="item" class="bg-teal-200 m-4 text-lg rounded-box font-semibold">
+                <p class="py-2 px-4 text-black">{item.type}</p>
+                <button><i class="fa-solid fa-pen-to-square text-black text-xl pr-8"></i></button>
+                <button><i class="fa-solid fa-arrow-up text-black text-xl pr-8"></i></button>
+                <button><i class="fa-solid fa-arrow-down text-black text-xl pr-7"></i></button>
+                <button><i class="fa-solid fa-x text-red-600 text-xl pr-4"></i></button>
+                </div>
+            {/each}
+        </div>
+        <div class="divider divider-horizontal"></div>
+        <div id="second" class="w-3/6">
+
+        </div>
+    </div>
+
+    <div id="container3">
+        <input type="number" placeholder="Number of test cases" class="input input-bordered max-w-xs border-teal-500" />
+        <button class="btn bg-teal-400 text-lg text-black hover:text-white hover:bg-teal-600 ml-4">Generate</button>
+        <button class="btn btn-outline border-teal-400 hover:bg-teal-600 text-lg hover:text-white">Download</button>
+    </div>
+
 </div>
 
 <style>
-    div{
-        background-color: rgb(30, 76, 90);
+    #container {
         height: 100vh;
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
+    }
+    #container2 {
+        display: flex;
+        padding: 0px 8px;
+        width: 50%;
+    }
+    #first{
+        overflow-y: auto;
+        height: 40vh;
+        overflow-x: hidden;
+    }
+    #second{
+        overflow-y: auto;
+        height: 40vh;
+        overflow-x: hidden;
+    }
+    #item{
+        display: flex;
+        align-items: center;
+        width: min-content;
+    }
+    #container3 {
+        display: flex;
+        padding: 0px 8px;
+        width: 50%;
+    }
+    #container3  button:last-child{
+        margin-left: auto;
     }
 </style>
---
+++ src/routes/nothing/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+    import { writableArray } from "$lib/store/list-store.js";
+
+    const addToArray = () => {
+        $writableArray = [
+            ...$writableArray,
+            {
+                type: "Integer",
+                value: 1,
+            },
+        ];
+    };
+
+    let dialog;
+    const open = () => {
+        console.log("wwww");
+        dialog.show();
+    };
+    const close = () => {
+        dialog.close();
+    };
+</script>
+
+<div id="container">
+    <dialog bind:this={dialog}>
+        <h1>Opend</h1>
+        <form>
+            min = <input type="number" />
+            max = <input type="number" />
+            <button on:click={close}>Save</button>
+        </form>
+    </dialog>
+    <button on:click={open}>Open</button>
+
+    <button on:click={addToArray}> Add item </button>
+    {#each $writableArray as item}
+        <h1>
+            {item.type}
+        </h1>
+        <p>
+            {item.value}
+        </p>
+    {/each}
+</div>
+
+<style>
+    #container {
+        background-color: rgb(69, 101, 110);
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    dialog {
+        text-align: center;
+        width: 200px;
+        padding: 8px;
+        opacity: 0;
+        scale: 0;
+        display: block;
+        transition: all 0.5s;
+    }
+    dialog[open] {
+        opacity: 1;
+        scale: 2;
+    }
+</style>
